d7cfad55b9046debc13a644f7e5d8a73197d8ec9
Mrawan Taha <mrawantahha@gmail.com>
2025-05-10 02:21:30 +0300

    adding styles and components

---
+++ src/components/UI/Button.tsx
@@ -0,0 +1,17 @@
+import type { ReactNode, ButtonHTMLAttributes } from "react";
+
+interface IProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+    className?: string;
+    children: ReactNode;
+    width?: "w-full" | "w-fit";
+}
+
+const Button = ({ className = "", children, width = "w-full", ...rest }: IProps) => {
+    return (
+        <button className={`${className} ${width} p-2 w-full rounded-md text-white cursor-pointer`} {...rest}>
+            {children}
+        </button>
+    );
+};
+
+export default Button;
---
+++ src/utils/functions.ts
@@ -0,0 +1,11 @@
+/**
+ * Truncates a given string to a specified maximum length and appends ellipsis if it exceeds the limit.
+ *
+ * @param {string} txt - The text string to be truncated.
+ * @param {number} [maxLen=50] - The maximum allowed length of the text. Defaults to 50.
+ * @returns {string} The original text if within the limit, or a truncated version followed by "...".
+ */
+export function textSlicer(txt: string, maxLen: number = 80) {
+    if (txt.length >= maxLen) return `${txt.slice(0, maxLen)}..`;
+    return txt
+}
